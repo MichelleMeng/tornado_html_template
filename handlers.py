9d4acf23b6f9406a3fd8010ee6cea56e0c7cbc22
meng <meng@MacBook-Pro.local>
2016-08-25 00:10:39 +0800

    first commit

---
+++ handlers.py
@@ -0,0 +1,10 @@
+# coding:utf8
+
+from views import *
+import os.path
+
+TEMPLATE_PATH = os.path.join(os.path.dirname(__file__),'templates')
+
+HANDLERS = [(r'/', IndexHandler), (r'/poem', PoemPageHandler)]
+
+HANDLERS += [(r'/show', ShowHandler)]
